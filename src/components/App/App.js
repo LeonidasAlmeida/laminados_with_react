97b758f230c129c60943262c629e78d78907a838
Leonidas Almeida <Leonidas.Almeida@promasidor.local>
2024-11-04 13:15:11 +0100

    pasta componente adicionada

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import Teste from "../Teste/Teste";
+
+function App() {
+  return (
+    <div>
+          ola mundo !
+          <Teste />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Teste/Teste.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Teste =()=>{
+
+    return(
+        <>
+           <p>TESTE FUNCIONANDO ! </p>
+        </>
+    );
+
+}
+
+export default Teste;
